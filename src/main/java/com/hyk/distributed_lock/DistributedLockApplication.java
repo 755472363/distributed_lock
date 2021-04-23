d9ef7b8c9b8c439d0e901365f816b667aa9e1646
haoyukui <haoyukui@58.com>
2021-04-23 14:27:06 +0800

    commit

---
+++ src/main/java/com/hyk/distributed_lock/DistributedLockApplication.java
@@ -0,0 +1,41 @@
+package com.hyk.distributed_lock;
+
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@SpringBootApplication
+public class DistributedLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedLockApplication.class, args);
+    }
+
+    @Bean
+    public Redisson redisson() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setDatabase(0);
+        return (Redisson)Redisson.create(config);
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean(StringRedisTemplate.class)
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
+        return stringRedisTemplate;
+    }
+
+
+}
