59f4cf11c888ed38d71ba8982d789e989d7748df
haoyukui <haoyukui@58.com>
2021-05-10 16:57:08 +0800

    commit

--- src/main/java/com/hyk/distributed_lock/DistributedLockApplication.java
+++ src/main/java/com/hyk/distributed_lock/DistributedLockApplication.java
@@ -1,13 +1,15 @@
 package com.hyk.distributed_lock;
 
 import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -20,14 +22,6 @@ public class DistributedLockApplication {
     }
 
     @Bean
-    public Redisson redisson() {
-        Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setDatabase(0);
-        return (Redisson)Redisson.create(config);
-    }
-
-
-    @Bean
     @ConditionalOnMissingBean(StringRedisTemplate.class)
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
@@ -35,6 +29,18 @@ public class DistributedLockApplication {
         stringRedisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
         return stringRedisTemplate;
+    }
+
+    @Autowired
+    private RedisProperties redisProperties;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        String redisUrl = String.format("redis://%s:%s", redisProperties.getHost() + "", redisProperties.getPort() + "");
+        config.useSingleServer().setAddress(redisUrl).setPassword(redisProperties.getPassword());
+        config.useSingleServer().setDatabase(3);
+        return Redisson.create(config);
     }
 
 
--- src/main/java/com/hyk/distributed_lock/demo/IndexController.java
+++ src/main/java/com/hyk/distributed_lock/demo/IndexController.java
@@ -1,28 +1,35 @@
 package com.hyk.distributed_lock.demo;
 
-import org.redisson.Redisson;
 import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-
 @RestController
 public class IndexController {
 
     @Autowired
-    private Redisson redisson;
+    private RedissonClient redisson;
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    private void setStock() throws Exception {
+        Integer stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock_hyk"));
+        if(stock != null && stock < 0){
+            stringRedisTemplate.opsForValue().set("stock_hyk", "10");
+        }else {
+            System.out.println("======="+stock);
+        }
+    }
+
     @RequestMapping("/deduct_stock")
     public String deductStock() throws Exception {
-        version1();
-        version2();
+//        setStock();
+        version8();
+//        version2();
         return "end";
     }
 
@@ -31,190 +38,190 @@ public class IndexController {
      * 最初版本，多线程情况下，会有线程并发问题：
      */
     private void version1() throws Exception {
-        int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
+        int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock_hyk"));
         if (stock > 0) {
             int realStock = stock - 1;
-            stringRedisTemplate.opsForValue().set("stock", realStock + "");
+            stringRedisTemplate.opsForValue().set("stock_hyk", realStock + "");
             System.out.println("扣除库存成功，剩余库存：" + realStock);
         } else {
             System.out.println("扣除库存失败，库存不足");
         }
     }
 
-    /**
-     * 加上jvm级别的锁，多个线程来时，可以解决并发问题：
-     * 但是在分布式环境下，还是会有并发问题：多个并发，轮询去分发请求
-     */
-    private void version2() throws Exception {
-        synchronized (this) {
-            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
-            if (stock > 0) {
-                int realStock = stock - 1;
-                stringRedisTemplate.opsForValue().set("stock", realStock + "");
-                System.out.println("扣除库存成功，剩余库存：" + realStock);
-            } else {
-                System.out.println("扣除库存失败，库存不足");
-            }
-        }
-    }
-
-    /**
-     * 用setNX    http://doc.redisfans.com/
-     * <p>
-     * SETNX key value
-     * 将 key 的值设为 value ，当且仅当 key 不存在。
-     * 若给定的 key 已经存在，则 SETNX 不做任何动作。
-     * 返回值：
-     * 设置成功，返回 1 。
-     * 设置失败，返回 0 。
-     * <p>
-     * <p>
-     * 这样就可以解决分布式高并发问题：
-     * <p>
-     * 但是有问题：异常，死锁等；
-     * 抛异常了，线程中断，key一直存在，则发生死锁；后边的减库存一直存在；
-     */
-    private void version3() throws Exception {
-        String lockKey = "stock_key";
-        Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, "stock_keyValue");
-        if (!result) {
-            System.out.println("拿不到锁，直接返回");
-            return;
-        }
-
-        int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
-        if (stock > 0) {
-            int realStock = stock - 1;
-            stringRedisTemplate.opsForValue().set("stock", realStock + "");
-            System.out.println("扣除库存成功，剩余库存：" + realStock);
-        } else {
-            System.out.println("扣除库存失败，库存不足");
-        }
-        Boolean delRst = stringRedisTemplate.delete(lockKey);
-        System.out.println("删除锁:" + (delRst ? "成功" : "失败"));
-    }
-
-
-    /**
-     * 解决死锁方案，try  finally:
-     * 问题：代码执行到半中间，挂了，比如重启服务（睡眠10s模拟），finally没有执行到，又发生死锁；
-     */
-    private void version4() throws Exception {
-        String lockKey = "stock_key";
-        try {
-            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, "stock_keyValue");
-            if (!result) {
-                System.out.println("拿不到锁，直接返回");
-                return;
-            }
-            //执行到这里服务挂了
-            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
-            if (stock > 0) {
-                int realStock = stock - 1;
-                stringRedisTemplate.opsForValue().set("stock", realStock + "");
-                System.out.println("扣除库存成功，剩余库存：" + realStock);
-            } else {
-                System.out.println("扣除库存失败，库存不足");
-            }
-        } finally {
-            Boolean delRst = stringRedisTemplate.delete(lockKey);
-            System.out.println("删除锁:" + (delRst ? "成功" : "失败"));
-        }
-    }
-
-    /**
-     * 解决方案，锁添加超时时间；
-     * 如果执行到了setIfAbsent，挂了，失效时间没执行，又发生死锁；
-     */
-    private void version5() throws Exception {
-        String lockKey = "stock_key";
-        try {
-            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, "stock_keyValue");
-            //执行到这里服务挂了
-            stringRedisTemplate.expire(lockKey, 10, TimeUnit.SECONDS);
-            if (!result) {
-                System.out.println("拿不到锁，直接返回");
-                return;
-            }
-
-            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
-            if (stock > 0) {
-                int realStock = stock - 1;
-                stringRedisTemplate.opsForValue().set("stock", realStock + "");
-                System.out.println("扣除库存成功，剩余库存：" + realStock);
-            } else {
-                System.out.println("扣除库存失败，库存不足");
-            }
-        } finally {
-            Boolean delRst = stringRedisTemplate.delete(lockKey);
-            System.out.println("删除锁:" + (delRst ? "成功" : "失败"));
-        }
-    }
-
-
-    /**
-     * 解决方案：高版本redis把两行代码，原子化，不会被打断，都要执行；
-     * <p>
-     * 还会存在问题：线程B的锁，被线程A释放到，导致锁失效；
-     * 过期时间10秒，第一个线程要执行15秒，执行10秒后超时，第二线程过来，需要执行10秒，
-     * 15秒后，第一个线程释放第二个线程的锁，导致锁失效；
-     */
-    private void version6() throws Exception {
-        String lockKey = "stock_key";
-        try {
-            //解决方案：高版本redis把两行代码，原子化，不会被打断，都要执行；
-            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, "stock_keyValue", 10, TimeUnit.SECONDS);
-            if (!result) {
-                System.out.println("拿不到锁，直接返回");
-                return;
-            }
-
-            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
-            if (stock > 0) {
-                int realStock = stock - 1;
-                stringRedisTemplate.opsForValue().set("stock", realStock + "");
-                System.out.println("扣除库存成功，剩余库存：" + realStock);
-            } else {
-                System.out.println("扣除库存失败，库存不足");
-            }
-        } finally {
-            Boolean delRst = stringRedisTemplate.delete(lockKey);
-            System.out.println("删除锁:" + (delRst ? "成功" : "失败"));
-        }
-    }
-
-    /**
-     * 解决方式：通过stock_keyValue隔离，可以解决的问题
-     * 但是超时问题还是无法解决；还是可能存在并发执行原子的代码段问题；(极限例子：失效时间1s,多个线程过来，多次操作数据库，有数据库io，导致多个线程操作，还存在原子代码段问题)
-     * lockKey是一样的，多个请求针对同一个lockKey会被限制住
-     */
-    private void version7() throws Exception {
-        String lockKey = "stock_key";
-        String clientId = UUID.randomUUID().toString();
-        try {
-            //解决方案：高版本redis把两行代码，原子化，不会被打断，都要执行；
-            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, clientId, 10, TimeUnit.SECONDS);
-            if (!result) {
-                System.out.println("拿不到锁，直接返回");
-                return;
-            }
-
-            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
-            if (stock > 0) {
-                int realStock = stock - 1;
-                stringRedisTemplate.opsForValue().set("stock", realStock + "");
-                System.out.println("扣除库存成功，剩余库存：" + realStock);
-            } else {
-                System.out.println("扣除库存失败，库存不足");
-            }
-        } finally {
-            if (clientId.equals(stringRedisTemplate.opsForValue().get(lockKey))) {
-                Boolean delRst = stringRedisTemplate.delete(lockKey);
-                System.out.println("删除锁:" + (delRst ? "成功" : "失败"));
-            }
-        }
-    }
-
+//    /**
+//     * 加上jvm级别的锁，多个线程来时，可以解决并发问题：
+//     * 但是在分布式环境下，还是会有并发问题：多个并发，轮询去分发请求
+//     */
+//    private void version2() throws Exception {
+//        synchronized (this) {
+//            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock_hyk"));
+//            if (stock > 0) {
+//                int realStock = stock - 1;
+//                stringRedisTemplate.opsForValue().set("stock_hyk", realStock + "");
+//                System.out.println("扣除库存成功，剩余库存：" + realStock);
+//            } else {
+//                System.out.println("扣除库存失败，库存不足");
+//            }
+//        }
+//    }
+//
+//    /**
+//     * 用setNX    http://doc.redisfans.com/
+//     * <p>
+//     * SETNX key value
+//     * 将 key 的值设为 value ，当且仅当 key 不存在。
+//     * 若给定的 key 已经存在，则 SETNX 不做任何动作。
+//     * 返回值：
+//     * 设置成功，返回 1 。
+//     * 设置失败，返回 0 。
+//     * <p>
+//     * <p>
+//     * 这样就可以解决分布式高并发问题：
+//     * <p>
+//     * 但是有问题：异常，死锁等；
+//     * 抛异常了，线程中断，key一直存在，则发生死锁；后边的减库存一直存在；
+//     */
+//    private void version3() throws Exception {
+//        String lockKey = "stock_key";
+//        Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, "stock_keyValue");
+//        if (!result) {
+//            System.out.println("拿不到锁，直接返回");
+//            return;
+//        }
+//
+//        int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock_hyk"));
+//        if (stock > 0) {
+//            int realStock = stock - 1;
+//            stringRedisTemplate.opsForValue().set("stock_hyk", realStock + "");
+//            System.out.println("扣除库存成功，剩余库存：" + realStock);
+//        } else {
+//            System.out.println("扣除库存失败，库存不足");
+//        }
+//        Boolean delRst = stringRedisTemplate.delete(lockKey);
+//        System.out.println("删除锁:" + (delRst ? "成功" : "失败"));
+//    }
+//
+//
+//    /**
+//     * 解决死锁方案，try  finally:
+//     * 问题：代码执行到半中间，挂了，比如重启服务（睡眠10s模拟），finally没有执行到，又发生死锁；
+//     */
+//    private void version4() throws Exception {
+//        String lockKey = "stock_key";
+//        try {
+//            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, "stock_keyValue");
+//            if (!result) {
+//                System.out.println("拿不到锁，直接返回");
+//                return;
+//            }
+//            //执行到这里服务挂了
+//            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock_hyk"));
+//            if (stock > 0) {
+//                int realStock = stock - 1;
+//                stringRedisTemplate.opsForValue().set("stock_hyk", realStock + "");
+//                System.out.println("扣除库存成功，剩余库存：" + realStock);
+//            } else {
+//                System.out.println("扣除库存失败，库存不足");
+//            }
+//        } finally {
+//            Boolean delRst = stringRedisTemplate.delete(lockKey);
+//            System.out.println("删除锁:" + (delRst ? "成功" : "失败"));
+//        }
+//    }
+//
+//    /**
+//     * 解决方案，锁添加超时时间；
+//     * 如果执行到了setIfAbsent，挂了，失效时间没执行，又发生死锁；
+//     */
+//    private void version5() throws Exception {
+//        String lockKey = "stock_key";
+//        try {
+//            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, "stock_keyValue");
+//            //执行到这里服务挂了
+//            stringRedisTemplate.expire(lockKey, 10, TimeUnit.SECONDS);
+//            if (!result) {
+//                System.out.println("拿不到锁，直接返回");
+//                return;
+//            }
+//
+//            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock_hyk"));
+//            if (stock > 0) {
+//                int realStock = stock - 1;
+//                stringRedisTemplate.opsForValue().set("stock_hyk", realStock + "");
+//                System.out.println("扣除库存成功，剩余库存：" + realStock);
+//            } else {
+//                System.out.println("扣除库存失败，库存不足");
+//            }
+//        } finally {
+//            Boolean delRst = stringRedisTemplate.delete(lockKey);
+//            System.out.println("删除锁:" + (delRst ? "成功" : "失败"));
+//        }
+//    }
+//
+//
+//    /**
+//     * 解决方案：高版本redis把两行代码，原子化，不会被打断，都要执行；
+//     * <p>
+//     * 还会存在问题：线程B的锁，被线程A释放到，导致锁失效；
+//     * 过期时间10秒，第一个线程要执行15秒，执行10秒后超时，第二线程过来，需要执行10秒，
+//     * 15秒后，第一个线程释放第二个线程的锁，导致锁失效；
+//     */
+//    private void version6() throws Exception {
+//        String lockKey = "stock_key";
+//        try {
+//            //解决方案：高版本redis把两行代码，原子化，不会被打断，都要执行；
+//            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, "stock_keyValue", 10, TimeUnit.SECONDS);
+//            if (!result) {
+//                System.out.println("拿不到锁，直接返回");
+//                return;
+//            }
+//
+//            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock_hyk"));
+//            if (stock > 0) {
+//                int realStock = stock - 1;
+//                stringRedisTemplate.opsForValue().set("stock_hyk", realStock + "");
+//                System.out.println("扣除库存成功，剩余库存：" + realStock);
+//            } else {
+//                System.out.println("扣除库存失败，库存不足");
+//            }
+//        } finally {
+//            Boolean delRst = stringRedisTemplate.delete(lockKey);
+//            System.out.println("删除锁:" + (delRst ? "成功" : "失败"));
+//        }
+//    }
+//
+//    /**
+//     * 解决方式：通过stock_keyValue隔离，可以解决的问题
+//     * 但是超时问题还是无法解决；还是可能存在并发执行原子的代码段问题；(极限例子：失效时间1s,多个线程过来，多次操作数据库，有数据库io，导致多个线程操作，还存在原子代码段问题)
+//     * lockKey是一样的，多个请求针对同一个lockKey会被限制住
+//     */
+//    private void version7() throws Exception {
+//        String lockKey = "stock_key";
+//        String clientId = UUID.randomUUID().toString();
+//        try {
+//            //解决方案：高版本redis把两行代码，原子化，不会被打断，都要执行；
+//            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, clientId, 10, TimeUnit.SECONDS);
+//            if (!result) {
+//                System.out.println("拿不到锁，直接返回");
+//                return;
+//            }
+//
+//            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock_hyk"));
+//            if (stock > 0) {
+//                int realStock = stock - 1;
+//                stringRedisTemplate.opsForValue().set("stock_hyk", realStock + "");
+//                System.out.println("扣除库存成功，剩余库存：" + realStock);
+//            } else {
+//                System.out.println("扣除库存失败，库存不足");
+//            }
+//        } finally {
+//            if (clientId.equals(stringRedisTemplate.opsForValue().get(lockKey))) {
+//                Boolean delRst = stringRedisTemplate.delete(lockKey);
+//                System.out.println("删除锁:" + (delRst ? "成功" : "失败"));
+//            }
+//        }
+//    }
+//
     /**
      * 使用，三行代码搞定，底层帮我实现了刚才的逻辑；
      * lock.tryLock():默认30秒超时，后台开线程10秒，续命；
@@ -227,10 +234,10 @@ public class IndexController {
         try {
             lock.tryLock();
 
-            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
+            int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock_hyk"));
             if (stock > 0) {
                 int realStock = stock - 1;
-                stringRedisTemplate.opsForValue().set("stock", realStock + "");
+                stringRedisTemplate.opsForValue().set("stock_hyk", realStock + "");
                 System.out.println("扣除库存成功，剩余库存：" + realStock);
             } else {
                 System.out.println("扣除库存失败，库存不足");
